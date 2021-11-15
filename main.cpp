f65b77b0590cbd6ffa66e5a83b3663f953e69b9d
paulo-rozatto <rozatto.paulo.victor@gmail.com>
2021-11-14 21:32:18 -0300

    pequenas otimizacoes, mas maior problema ainda nao resolvido

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 #include <fstream>
 #include <math.h>
 
+// #define BUFFER_SIZE 2048000
 #define BUFFER_SIZE 102400
-// #define BUFFER_SIZE 142
 #define ROWS 3646473
+// #define ROWS 400
 
 using namespace std;
 
@@ -35,7 +36,7 @@ bool writeID(char *buffer, int &pos, int length, fstream &file)
 
     int size = length - pos;
 
-    if (size < 89)
+    if (size <= 89)
     {
         needsNextBuffer = true;
     }
@@ -50,7 +51,7 @@ bool writeID(char *buffer, int &pos, int length, fstream &file)
     return needsNextBuffer;
 }
 
-bool writeReview(char *buffer, int &pos, int length, fstream &file, int &row)
+bool writeReview(char *buffer, int &pos, int length, fstream &file, unsigned long int &row)
 {
     bool needsNextBuffer = false;
     int i = 0, size;
@@ -84,7 +85,7 @@ bool writeReview(char *buffer, int &pos, int length, fstream &file, int &row)
     }
     else
     {
-        for (i += pos; buffer[i] != ','; i++)
+        for (i = pos; buffer[i] != ','; i++)
         {
             if (i == length)
             {
@@ -96,15 +97,28 @@ bool writeReview(char *buffer, int &pos, int length, fstream &file, int &row)
 
     if (!needsNextBuffer)
     {
-        size = i - pos;
+        // escreve enderco de onde esta o review
         file.write(reinterpret_cast<const char *>(&row), sizeof(row));
 
+        // salva o cursor atual do arquivo
         int aux = file.tellp();
+
+        // avanca no arquivo ate o local onde vai se colocar o comentario
         file.seekp(row, file.beg);
+
+        // calcula tamanho do comentario e salva o tamanho no arquivo
+        size = i - pos;
         file.write(reinterpret_cast<const char *>(&size), sizeof(size));
+
+        // escreve o comentario no arquivo
         file.write(buffer + pos, size * sizeof(char));
+
+        // calcula a posicao para o proximo review
         row += size + sizeof(size);
+
+        // volta o cursor do arquivo para o valor salvo
         file.seekp(aux, file.beg);
+
         pos = i + 1;
     }
 
@@ -118,23 +132,27 @@ bool writeUpVote(char *buffer, int &pos, int length, fstream &file)
 
     for (i = pos; buffer[i] != ','; i++)
     {
-        if (i == length)
+        if (i >= length)
         {
             needsNextBuffer = true;
             break;
         }
     }
+    if (!needsNextBuffer)
+    {
+        upvote = toInt((buffer + pos), (i - pos));
+        file.write(reinterpret_cast<const char *>(&upvote), sizeof(upvote));
+        pos = i + 1;
+    }
 
-    upvote = toInt((buffer + pos), (i - pos));
-    file.write(reinterpret_cast<const char *>(&upvote), sizeof(upvote));
-    pos = i + 1;
     return needsNextBuffer;
 }
 
 bool writeVersion(char *buffer, int &pos, int length, fstream &file)
 {
     bool needsNextBuffer = false;
-    int i, numb, count = 0;
+    int i, start = pos;
+    int v[3] = {0, 0, 0}, idx = 0;
 
     for (i = pos; buffer[i] != ','; i++)
     {
@@ -146,24 +164,20 @@ bool writeVersion(char *buffer, int &pos, int length, fstream &file)
 
         if (buffer[i] == '.')
         {
-            numb = toInt((buffer + pos), (i - pos));
-            file.write(reinterpret_cast<const char *>(&numb), sizeof(numb));
-            pos = i + 1;
-            count++;
+            v[idx] = toInt((buffer + start), (i - start));
+            idx += 1;
+            start = i + 1;
         }
     }
-    if (count == 2)
+    if (!needsNextBuffer)
     {
-        numb = toInt((buffer + pos), (i - pos));
-        file.write(reinterpret_cast<const char *>(&numb), sizeof(numb));
+        v[idx] = toInt((buffer + start), (i - start));
+
+        for (int j = 0; j < 3; j++)
+            file.write(reinterpret_cast<const char *>(v + j), sizeof(int));
+
+        pos = i + 1;
     }
-    else
-    {
-        numb = 0;
-        for (int j = count; j < 3; j++)
-            file.write(reinterpret_cast<const char *>(&numb), sizeof(numb));
-    }
-    pos = i + 1;
 
     return needsNextBuffer;
 }
@@ -174,7 +188,7 @@ bool writeDate(char *buffer, int &pos, int length, fstream &file)
 
     int size = length - pos;
 
-    if (size < 19)
+    if (size <= 19)
     {
         needsNextBuffer = true;
     }
@@ -199,7 +213,7 @@ void readToBuffer(char *buffer, int &length, fstream &file, int &pos)
     if (length > BUFFER_SIZE)
         length = BUFFER_SIZE;
 
-    file.seekg(pos, file.beg);
+    file.seekg(pos);
     file.read(buffer, length);
 }
 
@@ -222,6 +236,8 @@ void process()
     int length = csv.tellg();
     csv.seekg(54, csv.beg);
 
+    int test = length;
+
     if (length > BUFFER_SIZE)
         length = BUFFER_SIZE;
 
@@ -229,13 +245,12 @@ void process()
 
     csv.read(buffer, length);
 
-    char test = buffer[0];
-
     // id (89 char) + review position (1 int) + upvote (1 int) + version (3 int) + date (19 char)
-    const int ROW_SIZE = 89 * sizeof(char) + sizeof(int) + sizeof(int) + 3 * sizeof(int) + 19 * sizeof(char);
-    int rowPos = ROW_SIZE * ROWS;
-    int next = ID, pos = 0, filePos = 0;
+    const int ROW_SIZE = 89 * sizeof(char) + sizeof(unsigned long int) + sizeof(int) + 3 * sizeof(int) + 19 * sizeof(char);
+    unsigned long int rowPos = ROW_SIZE * ROWS;
+    int next = ID, pos = 0, filePos = 54;
     bool needsNextBuffer;
+    int auxCont = 0;
 
     while (pos < length)
     {
@@ -243,6 +258,7 @@ void process()
         {
         case ID:
         {
+            auxCont++;
             needsNextBuffer = writeID(buffer, pos, length, out);
             if (needsNextBuffer)
                 break;
@@ -287,6 +303,11 @@ void process()
         }
         }
 
+        if (pos >= length && csv.tellg() < test)
+        {
+            needsNextBuffer = true;
+        }
+
         if (needsNextBuffer)
         {
             filePos += pos;
@@ -295,14 +316,41 @@ void process()
         }
     }
 
-    // writeID(buffer, pos, length, out);
-    // writeReview(buffer, pos, length, out);
-    // writeUpVote(buffer, pos, length, out);
-    // writeVersion(buffer, pos, length, out);
-    // writeDate(buffer, pos, length, out);
-
     csv.close();
     delete[] buffer;
+}
+
+void test()
+{
+    fstream bin;
+    // id (89 char) + review position (1 int) + upvote (1 int) + version (3 int) + date (19 char)
+    const int ROW_SIZE = 89 * sizeof(char) + sizeof(unsigned long int) + sizeof(int) + 3 * sizeof(int) + 19 * sizeof(char);
+
+    bin.open("archive/tiktok_app_reviews.bin", ios::in | ios::binary);
+    char id[89], date[19];
+    unsigned long int position;
+    int upvote, version[3], size;
+    int i = 3646475, aux;
+    i *= ROW_SIZE;
+
+    bin.seekg(i, bin.beg);
+    aux = bin.tellg();
+
+    bin.read(id, 89 * sizeof(char));
+    bin.read(reinterpret_cast<char *>(&position), sizeof(position));
+    bin.read(reinterpret_cast<char *>(&upvote), sizeof(upvote));
+    bin.read(reinterpret_cast<char *>(version), sizeof(version));
+    bin.read(date, 19 * sizeof(char));
+
+    bin.seekg(position, bin.beg);
+    bin.read(reinterpret_cast<char *>(&size), sizeof(size));
+
+    char *review = new char[size];
+
+    bin.read(review, size * sizeof(char));
+
+    bin.close();
+    delete[] review;
 }
 
 int main(int argc, char const *argv[])
@@ -325,6 +373,7 @@ int main(int argc, char const *argv[])
             break;
         case '1':
             process();
+            // test();
             break;
         default:
             cout << "Opcao invalida" << endl;
