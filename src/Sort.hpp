bad46b33fa0d31e92a167b73006eeb2b3aa3f1d4
André Luiz Cunha de Oliveira <54187669+andrelcol@users.noreply.github.com>
2021-12-23 22:38:20 -0300

    hashing

--- src/Sort.hpp
+++ src/Sort.hpp
@@ -24,7 +24,7 @@ void swap(Register **a, Register **b)
 
 void insertionSort(Register **v, int begin, int end, int *contMov, int *contComp)
 {
-    for (int i = begin; i < end - 1; i++)
+    for (int i = begin; i < end; i++)
     {
         int j = i + 1;
         Register *pivot = v[j];
@@ -46,19 +46,24 @@ void insertionSort(Register **v, int begin, int end, int *contMov, int *contComp
 
 int partition(Register **v, int p, int q, int *contMov, int *contComp)
 {
-    int i = p, j = q - 1, pivot;
+    int i = p, j = q, pivot;
+    // int pivot = v[(p + q) / 2]->getUpvote();
 
     // mediana de tres
-    int mid = v[(p + q) / 2]->getUpvote();
-    if (v[p]->getUpvote() > v[j]->getUpvote())
-        swap(&v[p], &v[j]);
-    if (v[mid]->getUpvote() > v[j]->getUpvote())
-        swap(&v[mid], &v[j]);
+    int mid = (p + q) / 2;
+
+    if (v[p]->getUpvote() > v[j - 1]->getUpvote())
+        swap(&v[p], &v[j - 1]);
+
+    if (v[mid]->getUpvote() > v[j - 1]->getUpvote())
+        swap(&v[mid], &v[j - 1]);
+
     if (v[p]->getUpvote() > v[mid]->getUpvote())
         swap(&v[p], &v[mid]);
-    swap(&v[mid], &v[j]);
 
-    pivot = v[j]->getUpvote();
+    swap(&v[mid], &v[j - 1]);
+
+    pivot = v[j - 1]->getUpvote();
 
     while (true)
     {
@@ -76,13 +81,14 @@ int partition(Register **v, int p, int q, int *contMov, int *contComp)
             *contComp += 1;
         }
 
-        if (i < j)
+        if (i <= j)
         {
             swap(&v[i], &v[j]);
             *contMov += 1;
-
+            // if (i < q)
             i++;
-            j--;
+            if (j > p)
+                j--;
         }
         else
         {
@@ -143,33 +149,19 @@ void heapify(Register **v, int root, int n, int *contMov, int *contComp)
     }
 }
 
-void heapSort(Register **v, int n, int *contMov, int *contComp)
-{
-    // constroi heap
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(v, i, n, contMov, contComp);
-
-    while (n > 0)
-    {
-        n--;
-        swap(v[0], v[n]);
-        *contMov += 1;
-        heapify(v, 0, n, contMov, contComp);
-    }
-}
-
 void heapSort(Register **v, int begin, int end, int *contMov, int *contComp)
 {
     int size = end - begin;
 
     for (int i = size / 2 - 1 + begin; i >= begin; i--)
-        heapify(v, i, end, contMov, contComp);
+        heapify(v, i, end + 1, contMov, contComp);
 
     while (end > begin)
     {
-        end--;
         swap(v[begin], v[end]);
         heapify(v, begin, end, contMov, contComp);
+        *contMov += 1;
+        end--;
     }
 }
 
@@ -191,6 +183,7 @@ void introSortRec(Register **v, int begin, int end, int depthLimit, int *contMov
         return;
     }
 
+    // int pivot = partition(v, begin, end, contMov, contComp);
     int pivot = partition(v, begin, end, contMov, contComp);
     depthLimit -= 1;
 
@@ -204,5 +197,233 @@ void introSort(Register **v, int begin, int end, int *contMov, int *contComp)
 
     introSortRec(v, begin, end, depthLimit, contMov, contComp);
 }
+
+
+// Intro sort para a tabela hash
+
+typedef struct {
+    int version[3];
+    int quant = 0;
+}versao;
+
+void swapHash(versao **a, versao **b)
+{
+    versao *aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+int partitionHash(versao **v, int p, int q, int *contMov, int *contComp)
+{
+    int i = p, j = q - 1, pivot;
+
+    // mediana de tres
+    int mid = v[(p + q) / 2]->quant;
+    if (v[p]->quant > v[j]->quant)
+        swapHash(&v[p], &v[j]);
+    if (v[mid]->quant > v[j]->quant)
+        swapHash(&v[mid], &v[j]);
+    if (v[p]->quant > v[mid]->quant)
+        swapHash(&v[p], &v[mid]);
+    swapHash(&v[mid], &v[j]);
+
+    pivot = v[j]->quant;
+
+    while (true)
+    {
+        *contComp += 1;
+        while (v[i]->quant < pivot)
+        {
+            i++;
+            *contComp += 1;
+        }
+
+        *contComp += 1;
+        while (v[j]->quant > pivot)
+        {
+            j--;
+            *contComp += 1;
+        }
+
+        if (i < j)
+        {
+            swapHash(&v[i], &v[j]);
+            *contMov += 1;
+
+            i++;
+            j--;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return j;
+}
+
+void quickSortHash(versao **v, int p, int r, int *contMov, int *contComp)
+{
+    int q;
+    if (p < r)
+    {
+        q = partitionHash(v, p, r, contMov, contComp);
+        quickSortHash(v, p, q, contMov, contComp);
+        quickSortHash(v, q + 1, r, contMov, contComp);
+    }
+    cout << v[0]->quant << endl;
+}
+
+/*
+int partitionHash(versao **v, int p, int q, int *contMov, int *contComp)
+{
+    int i = p, j = q - 1, pivot;
+
+    // mediana de tres
+    int mid = v[(p + q) / 2]->quant;
+    if (v[p]->quant > v[j]->quant)
+        swapHash(&v[p], &v[j]);
+    if (v[mid]->quant > v[j]->quant)
+        swapHash(&v[mid], &v[j]);
+    if (v[p]->quant > v[mid]->quant)
+        swapHash(&v[p], &v[mid]);
+    swapHash(&v[mid], &v[j]);
+
+    pivot = v[j]->quant;
+
+    while (true)
+    {
+        *contComp += 1;
+        while (v[i]->quant < pivot)
+        {
+            i++;
+            *contComp += 1;
+        }
+
+        *contComp += 1;
+        while (v[j]->quant > pivot)
+        {
+            j--;
+            *contComp += 1;
+        }
+
+        if (i < j)
+        {
+            swapHash(&v[i], &v[j]);
+            *contMov += 1;
+
+            i++;
+            j--;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return j;
+}
+
+void insertionSortHash(versao **v, int begin, int end, int *contMov, int *contComp)
+{
+    for (int i = begin; i < end - 1; i++)
+    {
+        int j = i + 1;
+        versao *pivot = v[j];
+
+        *contComp += 1;
+        while (j > 0 && pivot->quant < v[j - 1]->quant)
+        {
+            v[j] = v[j - 1];
+            *contComp += 1;
+            *contMov += 1;
+            j--;
+        }
+
+        v[j] = pivot;
+    }
+}
+
+void heapifyHash(versao **v, int root, int n, int *contMov, int *contComp)
+{
+
+    int child;
+    int max = root; // assume que a raiz e o maior valor
+
+    while (root < n)
+    {
+        child = 2 * root + 1;
+
+        *contComp += 1;
+        if (child < n && v[child]->quant > v[child]->quant)
+        {
+            max = child;
+            *contComp += 1;
+        }
+
+        child += 1;
+
+        *contComp += 1;
+        if (child < n && v[child]->quant > v[child]->quant)
+        {
+            max = child;
+            *contComp += 1;
+        }
+
+        if (max != root)
+        {
+            swapHash(&v[root], &v[max]);
+            *contMov += 1;
+        }
+        else
+            break;
+
+        root = max;
+    }
+}
+
+void heapSortHash(versao **v, int begin, int end, int *contMov, int *contComp)
+{
+    int size = end - begin;
+
+    for (int i = size / 2 - 1 + begin; i >= begin; i--)
+        heapifyHash(v, i, end, contMov, contComp);
+
+    while (end > begin)
+    {   
+        end--;
+        swapHash(&v[begin], &v[end]);
+        heapifyHash(v, begin, end, contMov, contComp);
+    }
+}
+
+void introSortRecHash(versao **v, int begin, int end, int depthLimit, int *contMov, int *contComp)
+{
+    int size = end - begin;
+
+    if (size < 16)
+    {
+        insertionSortHash(v, 0, 1000000, contMov, contComp);
+        return;
+    }
+
+    if (depthLimit == 0)
+    {
+        heapSortHash(v, begin, end, contMov, contComp);
+        return;
+    }
+
+    int pivot = partitionHash(v, begin, end, contMov, contComp);
+    depthLimit -= 1;
+
+    introSortRecHash(v, begin, pivot, depthLimit, contMov, contComp);
+    introSortRecHash(v, pivot + 1, end, depthLimit, contMov, contComp);
+}
+
+void introSortHash(versao **v, int begin, int end, int *contMov, int *contComp)
+{
+    int depthLimit = 2 * log(end - begin);
+
+    introSortRecHash(v, begin, end, depthLimit, contMov, contComp);
+}
+*/
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ void importacao(Register **registers, int n)
     for (int i = 0; i < n; i++)
     {
         j = rand() % ROWS;
-        // registers[i] = new Register(bin, j);
+        // j = i;
         registers[i]->init(bin, j);
     }
 }
@@ -89,7 +89,7 @@ void ordenacao()
 
         t1 = t2 = t3 = cm1 = cm2 = cm3 = cc1 = cc2 = cc3 = 0;
 
-        cout << "--- Teste para " << n << " valores. ---" << endl;
+        cout << "--- Ordenacao de " << n << " valores. ---" << endl;
         output << "----- " << n << " valores -----" << endl;
 
         for (int i = 0; i < m; i++)
@@ -108,7 +108,7 @@ void ordenacao()
             contMoves = 0, contComps = 0;
 
             start = std::chrono::system_clock::now();
-            quickSort(v1, 0, n, &contMoves, &contComps);
+            quickSort(v1, 0, n - 1, &contMoves, &contComps);
             end = std::chrono::system_clock::now();
 
             elapsed_seconds = end - start;
@@ -128,7 +128,7 @@ void ordenacao()
             contMoves = 0, contComps = 0;
 
             start = std::chrono::system_clock::now();
-            heapSort(v2, 0, n, &contMoves, &contComps);
+            heapSort(v2, 0, n - 1, &contMoves, &contComps);
             end = std::chrono::system_clock::now();
 
             elapsed_seconds = end - start;
@@ -149,7 +149,7 @@ void ordenacao()
             contMoves = 0, contComps = 0;
 
             start = std::chrono::system_clock::now();
-            introSort(v3, 0, n, &contMoves, &contComps);
+            introSort(v3, 0, n - 1, &contMoves, &contComps);
             end = std::chrono::system_clock::now();
 
             elapsed_seconds = end - start;
@@ -167,32 +167,18 @@ void ordenacao()
 
         output << "Médias" << endl;
 
-        // cout << "QuickSort " << endl
-        //      << "Movimentacoes: " << cm1 / (float)m << endl
-        //      << "Comparacoes: " << cc1 / (float)m << endl
-        //      << "Tempo: " << t1 / (float)m << endl
-        //      << endl;
-        // cout << "HeapSort " << endl
-        //      << "Movimentacoes: " << cm2 / (float)m << endl
-        //      << "Comparacoes: " << cc2 / (float)m << endl
-        //      << "Tempo: " << t2 / (float)m << endl
-        //      << endl;
-        // cout << "IntroSort: " << endl
-        //      << "Movimentacoes: " << cm3 / (float)m << endl
-        //      << "Comparacoes: " << cc3 / (float)m << endl
-        //      << "Tempo: " << t3 / (float)m << endl
-        //      << endl;
-
         output << "QuickSort: " << endl
                << "Movimentacoes: " << cm1 / (float)m << endl
                << "Comparacoes: " << cc1 / (float)m << endl
                << "Tempo: " << t1 / (float)m << endl
                << endl;
+
         output << "HeapSort: " << endl
                << "Movimentacoes: " << cm2 / (float)m << endl
                << "Comparacoes: " << cc2 / (float)m << endl
                << "Tempo: " << t2 / (float)m << endl
                << endl;
+
         output << "IntroSort: " << endl
                << "Movimentacoes: " << cm3 / (float)m << endl
                << "Comparacoes: " << cc3 / (float)m << endl
@@ -207,27 +193,493 @@ void ordenacao()
     }
 }
 
-void temporario()
+void testeOrdenacao(Register **v, int n, ofstream &output)
+{
+    Register **v1, **v2, **v3;
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    std::chrono::duration<double> elapsed_seconds;
+    int contMoves = 0, contComps = 0;
+    double t1, t2, t3;
+    int cm1 = 0, cm2 = 0, cm3 = 0, cc1 = 0, cc2 = 0, cc3 = 0;
+
+    v1 = createArray(n);
+    v2 = createArray(n);
+    v3 = createArray(n);
+    copyUpvotes(v, v1, n);
+    copyUpvotes(v, v2, n);
+    copyUpvotes(v, v3, n);
+
+    cout << "--- Teste de ordenacao de " << n << " valores. ---" << endl;
+    output << "----- Teste de ordenacao de " << n << " valores -----" << endl;
+
+    cout << "Quick sort... ";
+    output << "Quick sort" << endl;
+    contMoves = 0, contComps = 0;
+
+    start = std::chrono::system_clock::now();
+    quickSort(v1, 0, n - 1, &contMoves, &contComps);
+    //  quickSort(v1, 0, n - 1,  &contMov, &contComp);
+    end = std::chrono::system_clock::now();
+
+    elapsed_seconds = end - start;
+    t1 += elapsed_seconds.count();
+    cm1 += contMoves;
+    cc1 += contComps;
+
+    cout << "Concluindo em " << elapsed_seconds.count() << " segundos" << endl
+         << endl;
+
+    output << "Movimentacoes: " << contMoves << endl;
+    output << "Comparacoes: " << contComps << endl;
+    output << "Concluindo em " << elapsed_seconds.count() << " segundos" << endl
+           << endl;
+    cout << "Heap sort... ";
+    output << "Heap sort" << endl;
+    contMoves = 0, contComps = 0;
+
+    start = std::chrono::system_clock::now();
+    heapSort(v2, 0, n - 1, &contMoves, &contComps);
+    end = std::chrono::system_clock::now();
+
+    elapsed_seconds = end - start;
+    t2 += elapsed_seconds.count();
+    cm2 += contMoves;
+    cc2 += contComps;
+
+    cout << "Concluindo em " << elapsed_seconds.count() << " segundos" << endl
+         << endl;
+
+    output << "Movimentacoes: " << contMoves << endl;
+    output << "Comparacoes: " << contComps << endl;
+    output << "Concluindo em " << elapsed_seconds.count() << " segundos" << endl
+           << endl;
+
+    cout << "Intro sort... ";
+    output << "Intro sort" << endl;
+    contMoves = 0, contComps = 0;
+
+    start = std::chrono::system_clock::now();
+    introSort(v3, 0, n - 1, &contMoves, &contComps);
+    end = std::chrono::system_clock::now();
+
+    elapsed_seconds = end - start;
+    t3 += elapsed_seconds.count();
+    cm3 += contMoves;
+    cc3 += contComps;
+
+    cout << "Concluindo em " << elapsed_seconds.count() << " segundos" << endl
+         << endl;
+    output << "Movimentacoes: " << contMoves << endl;
+    output << "Comparacoes: " << contComps << endl;
+    output << "Concluindo em " << elapsed_seconds.count() << " segundos" << endl
+           << endl;
+
+    output << "Valores originais: ";
+    for (int i = 0; i < n; i++)
+        output << v[i]->getUpvote() << " ";
+    output << endl; 
+
+    output << "Ordenado por Quick Sort: ";
+    for (int i = 0; i < n; i++)
+        output << v1[i]->getUpvote() << " ";
+    output << endl;
+
+    output << "Ordenado por Heap Sort:  ";
+    for (int i = 0; i < n; i++)
+        output << v2[i]->getUpvote() << " ";
+    output << endl;
+
+    output << "Ordenado por Intro Sort: ";
+    for (int i = 0; i < n; i++)
+        output << v3[i]->getUpvote() << " ";
+    output << endl;
+
+    output << "----- Fim teste de ordenacao -----" << endl;
+
+    deleteArray(v1, n);
+    deleteArray(v2, n);
+    deleteArray(v3, n);
+}
+
+int proximoPrimo(int n)
+{
+	int divisores = 0;
+	int primo = 0;
+
+	for (primo = n; primo <= n*2; primo++)
+	{
+		divisores = 0;
+
+		for (int i = 2; i <= sqrt(primo); i++)
+		{
+			if (primo % i == 0)
+				divisores++;
+		}
+		if (divisores == 0)
+		{
+			break;
+		}
+	}
+	return primo;
+}
+
+int fHash(int version[3], int n)
+{
+    int num = version[0] * 100 + version[1] * 10 + version[2] * 1;
+    return num % n;
+}
+
+int fHash2(int version[3], int n)
+{
+    int num = version[0] * 100 + version[1] * 10 + version[2] * 1;
+    return 7 - (num % 7);
+}
+
+void deleteTabela(versao **tabela, int n)
+{
+    for (int i = 0; i < n; i++)
+        if(tabela[i] != nullptr)
+            delete tabela[i];
+
+    delete[] tabela;
+}
+
+
+
+
+
+bool verificaVersao(versao **tabela, int indice, int vet[3])
+{
+    if(tabela[indice]->version[0] == vet[0]
+        && tabela[indice]->version[1] == vet[1]
+        && tabela[indice]->version[2] == vet[2])
+        {
+            return true;
+        }
+        
+    else
+        return false;
+}
+
+void proximoVazio(versao **tabela,int ver[3], int tam, int verifica[1000000], int posI)
+{
+    int i = posI;
+    while(verifica[i] == 1)
+    {  
+        if(i >= tam - 1)
+        {
+            i = 0;
+        }
+        else
+        {
+            i++;
+        }
+        if(i == posI)
+        {
+            cout << "Tabela cheia" << endl;
+            break;
+        }
+
+    }
+    tabela[i] = new versao;
+    tabela[i]->version[0] = ver[0];
+    tabela[i]->version[1] = ver[1];
+    tabela[i]->version[2] = ver[2];
+    tabela[i]->quant ++;
+    verifica[i] = 1;
+}
+//função para colocar as versoes que foram adicionadas antes das posições vazias
+versao** removeNull(versao **tabela, int n)
+{
+    int aux = 0;
+
+         for(int i = 0; i < n; i++)
+        {
+            aux = 0;
+            if(tabela[i] == nullptr)
+            {
+                for(int j = i; j < n; j++)
+                {
+                    if(tabela[j] != nullptr)
+                    {
+                        tabela[i] = tabela[j];
+                        tabela[j] = nullptr;
+                        aux = 1;
+                    }
+                }
+                //caso não elemento tenha sido movido, termina a execução
+                if(aux == 0)
+                {
+                    i = n;
+                }
+            }
+        }
+    return tabela;
+   
+}
+
+void testeHash(Register **r, int n, ofstream &output)
+{
+
+/*
+    for (int i = 0; i < n; i++)
+    {
+        cout << r[i]->getVersion()[0] << " ";
+        cout << r[i]->getVersion()[1] << " ";
+        cout << r[i]->getVersion()[2] << " " << endl;
+    }
+*/
+
+    int op, imprime = 0;
+    cout << "Informe quantas versões você deseja visualizar." << endl;
+    do{
+        cin >> op;
+        if(op < 0)
+        {
+            cout << "A quantidade de versões precisa ser maior ou igual a 0." << endl;
+        }
+    } while (op < 0);
+
+    cout << "Gostaria de visualizar os elementos da tabela Hash? Se sim, digite 1" << endl;
+    cin >> imprime;
+    
+    int tam = proximoPrimo(n);
+            
+    versao **tabela = new versao *[tam];
+   
+    int h = 0, verifica[tam];
+
+    for(int i = 0; i < tam; i++)
+    {
+        verifica[i] = 0;
+        tabela[i] = nullptr;
+    }
+
+    int aux = 1, j = 0, count = 0;
+    for(int i = 0; i < n; i++)
+    {
+                //aux = 1;
+        h = fHash(r[i]->getVersion(),tam);
+        do
+        {
+
+            if(verifica[h] != 1)
+            {
+                tabela[h] = new versao;
+                tabela[h]->version[0] = r[i]->getVersion()[0];
+                tabela[h]->version[1] = r[i]->getVersion()[1];
+                tabela[h]->version[2] = r[i]->getVersion()[2];
+                tabela[h]->quant ++;
+                aux = 0;
+                verifica[h] = 1;
+                j = 0;
+                if(imprime == 1)
+                    cout << tabela[h]->version[0] << "." << tabela[h]->version[1] << "." << tabela[h]->version[2] << endl;
+            }
+                    
+                    
+            else
+            {
+
+                if(verificaVersao(tabela, h, r[i]->getVersion()))
+                {
+                    tabela[h]->quant++;               
+                        aux = 0;
+                    }
+                        
+                    else
+                    {        
+                        j++;
+                        h = (fHash(r[i]->getVersion(),tam) + j*fHash2(r[i]->getVersion(),tam));
+                        int verification = (fHash(r[i]->getVersion(),tam) + fHash2(r[i]->getVersion(),tam));
+                        if (verification == h)
+                        {
+                            count ++;
+                            if(count > 2)
+                            {
+                                        //cout << "ERRO, indice repetindo varias vezes" << endl;
+                                proximoVazio(tabela, r[i]->getVersion(), tam,verifica, h);
+                                aux = 0;
+                            }
+                        }   
+                    }
+            }
+                
+        } while( aux != 0);
+    }
+    tabela = removeNull(tabela, tam);
+    int vet[3], quantidade, k = 0;
+    for(int i = 0; i < op; i++)
+        {
+            k = i;
+                /*while(tabela[k]->quant < 1)
+                {
+                    k++;
+                    cout << k << " ";
+                }*/
+
+            if(tabela[k] != nullptr)
+            {
+                quantidade = tabela[k]->quant;
+                for(int j = 0; j < 3; j++)
+                {
+                    vet[j] = tabela[k]->version[j];
+                    cout << vet[j] << " ";
+                }
+                output << "Versão " << vet[0] << "." << vet[1] << "." << vet[2] << " " << quantidade << " vezes" << endl;
+            }
+        }
+    deleteArray(r, n);
+    deleteTabela(tabela, n);
+}
+
+void tabelaHash()
+{
+    ifstream input("input.dat");
+    ofstream output("saida_hash.txt");
+
+    if(!input.good())
+    {
+        cout << "Arquivo input.dat nao encontrado." << endl;
+        input.close();
+        return;
+    }
+
+    if(!output.good())
+    {
+        cout << "Arquivo saida_hash.txt nao pode ser criado" << endl;
+        output.close();
+        return;
+    }
+
+    int n, op;
+    cout << "Informe quantas versões você deseja visualizar." << endl;
+    do{
+        cin >> op;
+        if(op < 0)
+        {
+            cout << "A quantidade de versões precisa ser maior ou igual a 0." << endl;
+        }
+    } while (op < 0);
+
+    while(input >> n)
+    {
+        if(n < 1000000)
+        {
+            cout << "--- Teste para " << n << " valores. ---" << endl;
+            output << "----- " << n << " valores -----" << endl;
+            int tam = proximoPrimo(n);
+
+            Register **r = createArray(n);
+            importacao(r, n);
+
+            versao **tabela = new versao *[tam];
+
+            int h = 0, verifica[tam];
+
+            for(int i = 0; i < tam; i++)
+            {
+                verifica[i] = 0;
+                tabela[i] = nullptr;
+            }
+
+            int aux = 1, j = 0, count = 0;
+            for(int i = 0; i < n; i++)
+            {
+                //aux = 1;
+                h = fHash(r[i]->getVersion(),tam);
+                do
+                {
+
+                    if(verifica[h] != 1)
+                    {
+                        tabela[h] = new versao;
+                        tabela[h]->version[0] = r[i]->getVersion()[0];
+                        tabela[h]->version[1] = r[i]->getVersion()[1];
+                        tabela[h]->version[2] = r[i]->getVersion()[2];
+                        tabela[h]->quant ++;
+                        aux = 0;
+                        verifica[h] = 1;
+                        j = 0;
+                    }
+                    
+                    
+                    else
+                    {
+
+                        if(verificaVersao(tabela, h, r[i]->getVersion()))
+                        {
+                            tabela[h]->quant++;
+                            
+                            aux = 0;
+                        }
+                        
+                        else
+                        {
+                            
+                                j++;
+                                h = (fHash(r[i]->getVersion(),tam) + j*fHash2(r[i]->getVersion(),tam));
+                                int verification = (fHash(r[i]->getVersion(),tam) + fHash2(r[i]->getVersion(),tam));
+                                if (verification == h)
+                                {
+                                    count ++;
+                                    if(count > 2)
+                                    {
+                                        //cout << "ERRO, indice repetindo varias vezes" << endl;
+                                        proximoVazio(tabela, r[i]->getVersion(), tam,verifica, h);
+                                        aux = 0;
+                                    }
+                                }
+                            
+
+                        }
+                    }
+                
+                } while( aux != 0);
+            }
+            tabela = removeNull(tabela, tam);
+            int vet[3], quantidade, k = 0;
+            for(int i = 0; i < op; i++)
+            {
+                k = i;
+                /*while(tabela[k]->quant < 1)
+                {
+                    k++;
+                    cout << k << " ";
+                }*/
+
+                if(tabela[k] != nullptr)
+                {
+                    quantidade = tabela[k]->quant;
+                    for(int j = 0; j < 3; j++)
+                    {
+                        vet[j] = tabela[k]->version[j];
+                        cout << vet[j] << " ";
+                    }
+                    output << "Versão " << vet[0] << "." << vet[1] << "." << vet[2] << " " << quantidade << " vezes" << endl;
+                }
+            }
+            deleteArray(r, n);
+            deleteTabela(tabela, n);
+
+        }
+        
+    }
+}
+
+void teste()
 {
     int n = 100;
     Register **r = createArray(n);
-    int contMov = 0, contComp = 0;
-    // r = new Register *[n];
+    ofstream output("teste.txt");
+    ofstream outputHash("teste_Hash.txt");
 
     importacao(r, n);
 
-    for (int i = 0; i < n; i++)
-        cout << r[i]->getUpvote() << " ";
-    cout << endl;
+    testeOrdenacao(r, n, output);
+    testeHash(r, n, outputHash);
 
-    heapSort(r, 0, n, &contMov, &contComp);
-
-    for (int i = 0; i < n; i++)
-        cout << r[i]->getUpvote() << " ";
-    cout << endl;
-
-    cout << "Mov: " << contMov << " - "
-         << "Comp: " << contComp << endl;
+    deleteArray(r, n);
 }
 
 int main(int argc, char const *argv[])
@@ -287,10 +739,10 @@ int main(int argc, char const *argv[])
             ordenacao();
             break;
         case '2':
-            cout << "A fazer..." << endl;
+            tabelaHash();
             break;
         case '3':
-            cout << "A fazer..." << endl;
+            teste();
             break;
         default:
             cout << "Opcao invalida" << endl;
